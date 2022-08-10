cbbfc3754b4fbecfd39870649d1d5a84af4a49dd
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-08-10 15:29:16 +0700

    add new package

--- src/components/OnBoard.js
+++ src/components/OnBoard.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, SafeAreaView, TextInput, Button, TouchableOpacity} from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+import { Button } from "@react-native-material/core";
 import { StatusBar } from 'expo-status-bar';
 
 
