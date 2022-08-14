265d7002f122c6ab34600fecd6396724a0f524aa
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-08-14 22:53:47 +0700

    add redux-toolkit

--- App.js
+++ App.js
@@ -6,21 +6,25 @@ import React from 'react';
 import Toast from 'react-native-toast-message';
 import OnBoard from './src/components/OnBoard';
 import LogIn from './src/screen/LogIn';
+import { Provider } from 'react-redux';
+import { store } from './src/features/index.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
-            <NavigationContainer>
+            <Provider store={store}>
+                <NavigationContainer>
                     <TailwindProvider>
                         <Stack.Navigator screenOptions={{ headerMode: 'none' }}  screenOptions={{headerShown: false,}} initialRouteName="Login">
                             <Stack.Screen name="Login" component={LogIn} />
                             <Stack.Screen name="Home" component={OnBoard} />
-                        </Stack.Navigator>
-                    </TailwindProvider>
-                <Toast />
-            </NavigationContainer>
+                            </Stack.Navigator>
+                        </TailwindProvider>
+                    <Toast />
+                </NavigationContainer>
+            </Provider>
         </>
     );
 }
--- src/screen/LogIn.js
+++ src/screen/LogIn.js
@@ -2,12 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
 import { Button } from "@react-native-material/core";
 import { StatusBar } from 'expo-status-bar';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, selectCount } from '../features/counter/counterSlice';
 
 const LogIn = ({ navigation }) => {
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+    const handlerIncrement = () => {
+        dispatch(increment());
+    }
     return (
         <SafeAreaView className="flex-1 items-center justify-center">
             <Text className="text-red-900">Login</Text>
+            <Text>{count}</Text>
+            <View className="my-4">
+                <Button 
+                    title="increment"
+                    onPress={handlerIncrement}
+                    className="my-4"
+                />
+            </View>
             <Button
                 title="Go to Details"
                 onPress={() => navigation.navigate('Home')}
